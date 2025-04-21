0fc1ebcd36ca77ca67e56cca4deb0c25c366e2f8
Deepak Bajwa <masoomjaat861@gmail.com>
2025-04-22 01:19:10 +0530

    Update info.py

--- info.py
+++ info.py
@@ -15,7 +15,7 @@ def is_enabled(value, default):
 SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ.get('API_ID', '27484185'))
 API_HASH = environ.get('API_HASH', '7a52fad60fd16e779bb2324778e5d8da')
-BOT_TOKEN = environ.get('BOT_TOKEN', '8022877397:AAG_GXizV51nYPVjhikMv80Pkhh4YJShp44')
+BOT_TOKEN = environ.get('BOT_TOKEN', '8022877397:AAF-rqVMnsfcnQxS3l3mvYkOsYF_wDOaT9w')
 #---------------------------------------------------------------
 #---------------------------------------------------------------
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '7077129266').split()]
